34cdf009d44972ab4c8a9df2565056e1dd83ad55
lukepeters <lukepeters@ucds.email>
2017-01-16 14:35:22 +0000

    removed UC specific FromNexusRule and allowed rule generators to be configured by plugin users

--- src/main/groovy/uk/gov/dwp/buckify/BuckifyExtension.groovy
+++ src/main/groovy/uk/gov/dwp/buckify/BuckifyExtension.groovy
@@ -3,6 +3,7 @@ package uk.gov.dwp.buckify
 import org.gradle.api.Project
 import org.gradle.api.artifacts.ResolvedArtifact
 import org.gradle.api.plugins.GroovyPlugin
+import uk.gov.dwp.buckify.rules.Rule
 
 class BuckifyExtension {
 
@@ -12,19 +13,45 @@ class BuckifyExtension {
     String groovyTestRuleName = "groovy-test"
     String javaLibraryRuleName = "main"
     String javaTestRuleName = "test"
+    /**
+     Paths of any pre-existing rule files e.g. a common file with BUCK dependency rules
+     */
     List<String> preExistingRuleFiles = []
     boolean autoDeps = true
 
+    /**
+     Method used to name binary JAR rules
+     */
     Closure<String> binaryJarRuleName = { String dep -> ":$dep-mvn" }
+    /**
+     Predicate used to to determine if a Groovy library rule should be generated for this project
+     */
     Closure groovyLibraryPredicate = { Project project ->
         project.plugins.hasPlugin(GroovyPlugin) && project.file("src/main/groovy").exists()
     }
+    /**
+     Allows custom naming rules to be applied to certain artifcats
+     */
     Closure<String> nomenclature = { ResolvedArtifact artifact ->
         artifact.name + (artifact.classifier ? "-$artifact.classifier" : "")
     }
+    /**
+     Predicate used to decide if an artifact should be excluded from the generate BUCK file
+     */
     Closure<Boolean> excluded = { ResolvedArtifact artifact ->
         false
     }
+    /**
+     Allows users to define their own custom rule generators
+     */
+    Collection<Closure<Collection<? extends Rule>>> ruleGenerators = [
+            uk.gov.dwp.buckify.rules.JavaLibraryRule.generator,
+            uk.gov.dwp.buckify.rules.GroovyLibraryRule.generator,
+            uk.gov.dwp.buckify.rules.JavaTestRule.generator,
+            uk.gov.dwp.buckify.rules.GroovyTestRule.generator,
+            uk.gov.dwp.buckify.rules.PreBuiltJarRule.generator,
+            uk.gov.dwp.buckify.rules.RemoteFileRule.generator
+    ]
 
     static BuckifyExtension from(Project project) {
         project.extensions.findByType(BuckifyExtension) ?: project.extensions.create("buckify", BuckifyExtension)
--- src/main/groovy/uk/gov/dwp/buckify/BuckifyTask.groovy
+++ src/main/groovy/uk/gov/dwp/buckify/BuckifyTask.groovy
@@ -4,27 +4,12 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
 import uk.gov.dwp.buckify.dependencies.DependencyCache
-import uk.gov.dwp.buckify.rules.FromNexusRule
-import uk.gov.dwp.buckify.rules.GroovyLibraryRule
-import uk.gov.dwp.buckify.rules.GroovyTestRule
-import uk.gov.dwp.buckify.rules.JavaLibraryRule
-import uk.gov.dwp.buckify.rules.JavaTestRule
 import uk.gov.dwp.buckify.rules.PreExistingRules
 import uk.gov.dwp.buckify.rules.Rule
 
 class BuckifyTask extends DefaultTask {
 
     static final String NAME = "buckify"
-
-    def ruleGenerators = [
-            JavaLibraryRule.generator,
-            GroovyLibraryRule.generator,
-            JavaTestRule.generator,
-            GroovyTestRule.generator,
-            FromNexusRule.generator,
-//            PreBuiltJarRule.generator,
-//            RemoteFileRule.generator
-    ]
 
     @TaskAction
     public void convert() {
@@ -37,7 +22,7 @@ class BuckifyTask extends DefaultTask {
 
     List<Rule> createRules(Project project, PreExistingRules preExistingRules) {
         DependencyCache dependencies = new DependencyCache(project, preExistingRules)
-        ruleGenerators.collect { it(project, dependencies) }.flatten()
+        BuckifyExtension.from(project).ruleGenerators.collect { it(project, dependencies) }.flatten()
     }
 
 }
